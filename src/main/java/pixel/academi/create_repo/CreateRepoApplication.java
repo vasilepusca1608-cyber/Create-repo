e0a102d61683ad796578843cfcf307711c424b63
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-09 12:50:39 +0200

    Initial commit

---
+++ src/main/java/pixel/academi/create_repo/CreateRepoApplication.java
@@ -0,0 +1,13 @@
+package pixel.academi.create_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academi/create_repo/CreateRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academi.create_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
